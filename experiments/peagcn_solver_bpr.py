2233ee234596cccfba04f429fd523ef81a3a9961
ShyamArumugam <a.shyam14@yahoo.com>
2020-09-15 22:38:02 +0200

    uniform the arguments and comments for all experiments

--- experiments/peagcn_solver_bpr.py
+++ experiments/peagcn_solver_bpr.py
@@ -17,20 +17,20 @@ parser = argparse.ArgumentParser()
 
 # Dataset params
 parser.add_argument('--dataset', type=str, default='Movielens', help='')		#Movielens, Yelp
-parser.add_argument('--dataset_name', type=str, default='25m', help='')	#1m, 25m, latest-small
+parser.add_argument('--dataset_name', type=str, default='latest-small', help='')	#1m, 25m, latest-small
 parser.add_argument('--if_use_features', type=str, default='false', help='')
-parser.add_argument('--num_core', type=int, default=30, help='')			#10, 20(only for 25m)
-parser.add_argument('--num_feat_core', type=int, default=30, help='')			#10, 20(only for 25m)
+parser.add_argument('--num_core', type=int, default=10, help='')			#10(for others), 20(only for 25m)
+parser.add_argument('--num_feat_core', type=int, default=10, help='')
 parser.add_argument('--sampling_strategy', type=str, default='unseen', help='')		#unseen(for 1m,latest-small), random(for Yelp,25m)
-parser.add_argument('--entity_aware', type=str, default='true', help='')
+parser.add_argument('--entity_aware', type=str, default='false', help='')
 # Model params
 parser.add_argument('--dropout', type=float, default=0, help='')
-parser.add_argument('--emb_dim', type=int, default=64, help='')
-parser.add_argument('--repr_dim', type=int, default=16, help='')
-parser.add_argument('--hidden_size', type=int, default=64, help='')
-parser.add_argument('--meta_path_steps', type=str, default='2,2,2,2,2,2,2,2,2', help='')	#2,2,2,2,2,2,2,2,2,2(for 1m,25m) #2,2,2,2,2,2,2,2,2,2,2 (for yelp)
+parser.add_argument('--emb_dim', type=int, default=64, help='')		#64(for others), 32(only for 25m)
+parser.add_argument('--repr_dim', type=int, default=16, help='')        #16(for others), 8(only for 25m)
+parser.add_argument('--hidden_size', type=int, default=64, help='')     #64(for others), 16(only for 25m)
+parser.add_argument('--meta_path_steps', type=str, default='2,2,2,2,2,2,2,2,2', help='')	#2,2,2,2,2,2,2,2,2(for small) #2,2,2,2,2,2,2,2,2,2(for 1m,25m) #2,2,2,2,2,2,2,2,2,2,2 (for yelp)
 parser.add_argument('--channel_aggr', type=str, default='att', help='')
-parser.add_argument('--entity_aware_coff', type=float, default=0.01, help='')
+parser.add_argument('--entity_aware_coff', type=float, default=0.1, help='')
 
 # Train params
 parser.add_argument('--init_eval', type=str, default='false', help='')
@@ -38,17 +38,17 @@ parser.add_argument('--num_negative_samples', type=int, default=4, help='')
 parser.add_argument('--num_neg_candidates', type=int, default=99, help='')
 
 parser.add_argument('--device', type=str, default='cuda', help='')
-parser.add_argument('--gpu_idx', type=str, default='4', help='')
-parser.add_argument('--runs', type=int, default=1, help='')
-parser.add_argument('--epochs', type=int, default=30, help='')
-parser.add_argument('--batch_size', type=int, default=1024, help='')
+parser.add_argument('--gpu_idx', type=str, default='0', help='')
+parser.add_argument('--runs', type=int, default=5, help='')
+parser.add_argument('--epochs', type=int, default=30, help='')    #30(for others), 20(only for Yelp)
+parser.add_argument('--batch_size', type=int, default=1024, help='')    #1024(for others), 4096(only for 25m)
 parser.add_argument('--num_workers', type=int, default=12, help='')
 parser.add_argument('--opt', type=str, default='adam', help='')
 parser.add_argument('--lr', type=float, default=0.001, help='')
 parser.add_argument('--weight_decay', type=float, default=0, help='')
 parser.add_argument('--early_stopping', type=int, default=20, help='')
 parser.add_argument('--save_epochs', type=str, default='5,10,15,20,25', help='')
-parser.add_argument('--save_every_epoch', type=int, default=26, help='')
+parser.add_argument('--save_every_epoch', type=int, default=26, help='')        #26(for others), 16(only for Yelp)
 
 args = parser.parse_args()
 
