56e75290ef1eb229e0eda789c81a68d725d3af83
Shyam Arumugaswamy <a.shyam14@yahoo.com>
2020-11-28 01:43:27 +0100

    update parsers

--- graph_recsys_benchmark/parser/__init__.py
+++ graph_recsys_benchmark/parser/__init__.py
@@ -1,9 +1,8 @@
-from .ml import parse_ml1m, parse_ml25m, parse_mlsmall
+from .ml import parse_ml25m, parse_mlsmall
 from .yelp import parse_yelp
 
 __all__ = [
     'parse_mlsmall'
-    'parse_ml1m',
     'parse_ml25m',
     'parse_yelp'
 ]
